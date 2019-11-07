25a506e677122e2a46a2d6513c7314fabcaccf46
nikkisowell <nicolesowell.dev@outlook.com>
2019-11-07 15:55:46 -0600

    updated app

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import paintings from "./cards.json";
+import Scoreboard from "./components/Scoreboard";
+import Card from "./components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// shuffle upon each click
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+class App extends Component {
+  state = {
+    paintings,
+    score: 0,
+    topScore: 0,
+    showAlert: 0,
+    showSuccess: 0,
+    clickedpaintings: []
+  };
+
+  clickedImage = id => {
+    // assign the state of the empty array to a let to be updated
+    let clickedpaintings = this.state.clickedpaintings;
+    let score = this.state.score;
+    let topScore = this.state.topScore;
+    this.setState({
+      showAlert: 0
+    });
+
+    // if the clicked image has an id of the indexed paintings
+    if (clickedpaintings.indexOf(id) === -1) {
+      // push that id into that id into the array to be stored
+      clickedpaintings.push(id);
+      console.log(clickedpaintings);
+      // run the score function
+      this.handleIncrement();
+      // run the reshuffle function after each click
+      this.makeShuffle();
+    } else if (this.state.score === 12) {
+      // alert player wins
+      this.setState({
+        showSuccess: 1,
+        score: 0,
+        clickedpaintings: []
+      });
+    } else {
+       // alert player loss
+      this.setState({
+        score: 0,
+        clickedpaintings: []
+      });
+      console.log("duplicate");
+      this.setState({
+        showAlert: 1
+      });
+    }
+
+    if (score > topScore) {
+      this.setState({
+        topScore: score
+      });
+    }
+  };
+
+  // handleIncrement increases this.state.score by 1
+  handleIncrement = () => {
+    // setState updates a components states
+    this.setState({ score: this.state.score + 1 });
+  };
+
+  // shuffle up images
+  makeShuffle = () => {
+    this.setState({ paintings: shuffle(paintings) });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div
+          className="alert alert-danger"
+          style={{ opacity: this.state.showAlert }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Oops, you clicked a dupe. Try again!
+          </div>
+        <div
+          className="alert alert-success"
+          style={{ opacity: this.state.showSuccess }}
+        >
+         Awesome Sauce!
+          </div>
+        <Scoreboard
+          title="PHENOMENAL WOMEN"
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
+       
+        <div className="row">
+          {this.state.paintings.map(painting => (
+            <Card
+              key={painting.id}
+              id={painting.id}
+              artist={painting.artist}
+              image={painting.image}
+              clickedImage={this.clickedImage}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ clicky-game/src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./card.css";
+
+const Cards = props => (
+  <div className="card" onClick={() => props.clickedImage(props.id)}>
+    <div className="img-container">
+      <img alt={props.artist} src={props.image} />
+      <div className="overlay">
+        <div className="text">
+      {props.artist} 
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Cards;
